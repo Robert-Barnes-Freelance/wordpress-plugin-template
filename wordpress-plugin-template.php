5dbb6e4222844c2e0a06a32ad893d31cc08685c0
Robert Barnes <robertbarnes1981@googlemail.com>
2022-05-03 15:59:44 +0100

    Initial commit

---
+++ wordpress-plugin-template.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WordPress Plugin Template
+Plugin URI:  https://github.com/bobbarnes1981/WordPress-Plugin-Template
+Description: 
+Version:     0.0.1
+Author:      Robert Barnes
+Author URI:  http://
+*/
+
